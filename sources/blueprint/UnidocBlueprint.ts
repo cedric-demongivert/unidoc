837df3aeded408c533becca650f229457ed4f077
Cédric <cedric.demongivert@gmail.com>
2020-11-15 16:51:55 -0500

    better blueprint building

--- sources/blueprint/UnidocBlueprint.ts
+++ sources/blueprint/UnidocBlueprint.ts
@@ -37,30 +37,74 @@ export namespace UnidocBlueprint {
   export type Word = UnidocWordBlueprint
 
   export const tag = UnidocTagBlueprint.create
-  export const any = UnidocAnyBlueprint.create
   export const anything = UnidocAnythingBlueprint.create
   export const end = UnidocEndBlueprint.create
-  export const lienientSequence = UnidocLenientSequenceBlueprint.create
-  export const many = UnidocManyBlueprint.create
-  export const set = UnidocSetBlueprint.create
   export const tagEnd = UnidocTagEndBlueprint.create
   export const tagStart = UnidocTagStartBlueprint.create
   export const whitespace = UnidocWhitespaceBlueprint.create
   export const word = UnidocWordBlueprint.create
 
-  export function sequence(): UnidocBlueprint
-  export function sequence(...elements: UnidocSequentialBlueprint[]): UnidocSequentialBlueprint
-  export function sequence(...elements: UnidocSequentialBlueprint[]): UnidocSequentialBlueprint | UnidocBlueprint {
+  export function set(...elements: UnidocBlueprint[]): UnidocSetBlueprint {
+    const result: UnidocSetBlueprint = UnidocSetBlueprint.create()
+
     if (elements.length > 0) {
-      const result: UnidocSequentialBlueprint = elements[0]
-
-      for (let index = 1; index < elements.length; ++index) {
-        elements[index - 1].then(elements[index])
+      for (let index = 0; index < elements.length; ++index) {
+        result.ofContent(elements[index])
       }
-
-      return result
-    } else {
-      return UnidocBlueprint.end()
     }
+
+    return result
+  }
+
+  export function many(element?: UnidocBlueprint): UnidocManyBlueprint {
+    const result: UnidocManyBlueprint = UnidocManyBlueprint.create()
+
+    if (element) {
+      result.ofContent(element)
+    }
+
+    return result
+  }
+}
+
+export function any(...elements: UnidocBlueprint[]): UnidocAnyBlueprint {
+  const result: UnidocAnyBlueprint = UnidocAnyBlueprint.create()
+
+  if (elements.length > 0) {
+    for (let index = 0; index < elements.length; ++index) {
+      result.ofContent(elements[index])
+    }
+  }
+
+  return result
+}
+
+export function sequence(): UnidocBlueprint
+export function sequence(...elements: UnidocSequentialBlueprint[]): UnidocSequentialBlueprint
+export function sequence(...elements: UnidocSequentialBlueprint[]): UnidocSequentialBlueprint | UnidocBlueprint {
+  if (elements.length > 0) {
+    const result: UnidocSequentialBlueprint = elements[0]
+
+    for (let index = 1; index < elements.length; ++index) {
+      elements[index - 1].then(elements[index])
+    }
+
+    return result
+  } else {
+    return UnidocBlueprint.end()
+  }
+}
+
+export namespace sequence {
+  export function lenient(...elements: UnidocBlueprint[]): UnidocLenientSequenceBlueprint {
+    const result: UnidocLenientSequenceBlueprint = UnidocLenientSequenceBlueprint.create()
+
+    if (elements.length > 0) {
+      for (let index = 0; index < elements.length; ++index) {
+        result.ofContent(elements[index])
+      }
+    }
+
+    return result
   }
 }
